ed152e5d4d34f6c94b124eb139608d07e2aff207
L-Wiss <wassimbenameur@gmail.com>
2015-11-29 00:39:13 +0800

    adding replies

--- js/app.js
+++ js/app.js
@@ -4,11 +4,11 @@ var app = angular.module('reddit-camouflage',[])
   $routeProvider.
   when('/overflow', {
     templateUrl:"templates/overflow_template.html",
-    controller:'overflowController'   
+    controller:'overflowController'
   }).
   when('/post', {
     templateUrl:"templates/overflow_post_template.html",
-    controller:'overflowPostController'   
+    controller:'overflowPostController'
   }).
   otherwise({redirectTo:'/overflow',
 });
@@ -70,7 +70,7 @@ var app = angular.module('reddit-camouflage',[])
 })
 .controller('overflowPostController',function($scope, $routeParams){
 	//ToDo  get reddit post from url params
-	var url='https://www.reddit.com/r/gifs/comments/3ub1il/my_way_is_quicker/.json?3jsonp=?'
+	var url='https://www.reddit.com/r/nosleep/comments/3uiif7/we_went_to_the_hospital_on_thanksgiving_and_the/.json?3jsonp=?'
 	$scope.posts = [];
 	$scope.comments = [];
 	if(localStorage.getItem("overflow_title"))
@@ -81,37 +81,36 @@ var app = angular.module('reddit-camouflage',[])
 	$.get(url,{},function(response) {
 	  var postCommments = response[1].data.children;
 	  for (var i = 0; i < postCommments.length; i++) {
-	  	if(postCommments[i].data.author == "PM_ME_PAYPALMONEY")
-	  		console.log(postCommments[i].data);
+	  	//if(postCommments[i].data.author == "PM_ME_PAYPALMONEY")
 	  	$scope.comments.push({
 	  		author:postCommments[i].data.author,
 	  		body:postCommments[i].data.body,
+        replies:(postCommments[i].data.replies.data)?postCommments[i].data.replies.data.children:null,
 	  		score:postCommments[i].data.score
 	  	})
 	  };
-	  
+
       var subRedditsArray = response[0].data.children;
       for (var i = 0; i < subRedditsArray.length; i++) {
-			$scope.posts.push({
-				title:subRedditsArray[i].data.title,
-				author:subRedditsArray[i].data.author,
-				subreddit:subRedditsArray[i].data.subreddit,
-				domain:subRedditsArray[i].data.domain,
-				permalink:subRedditsArray[i].data.permalink,
-				score:subRedditsArray[i].data.score,
-				type:subRedditsArray[i].data.post_hint,
-				thumbnail:{thumbnail_url:subRedditsArray[i].data.thumbnail,width:subRedditsArray[i].data.width,height:subRedditsArray[i].data.height},
-				num_comments:subRedditsArray[i].data.num_comments,
-				selfText:subRedditsArray[i].data.selftext,
-				selfTextShort:subRedditsArray[i].data.selftext.substr(0, 500),
-				expanded:false,
-				url:subRedditsArray[i].data.url});
-		}
-	$scope.$apply();
+  			$scope.posts.push({
+  				title:subRedditsArray[i].data.title,
+  				author:subRedditsArray[i].data.author,
+  				subreddit:subRedditsArray[i].data.subreddit,
+  				domain:subRedditsArray[i].data.domain,
+  				permalink:subRedditsArray[i].data.permalink,
+  				score:subRedditsArray[i].data.score,
+  				type:subRedditsArray[i].data.post_hint,
+  				thumbnail:{thumbnail_url:subRedditsArray[i].data.thumbnail,width:subRedditsArray[i].data.width,height:subRedditsArray[i].data.height},
+  				num_comments:subRedditsArray[i].data.num_comments,
+  				selfText:subRedditsArray[i].data.selftext,
+  				selfTextShort:subRedditsArray[i].data.selftext.substr(0, 500),
+  				expanded:false,
+  				url:subRedditsArray[i].data.url});
+  		}
+      $scope.$apply();
     });
 
 	$scope.editTitle = function() {
-		console.log("edit title");
 		document.getElementById('subPost_title_content').style.display="none";
 		document.getElementById('subPost_title_content_input').style.display="initial";
 		document.getElementById('subPost_title_content_input').focus();

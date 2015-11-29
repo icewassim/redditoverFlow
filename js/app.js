b2f4afb1caeb9c4c9e5d8e5c896764982c0d4086
L-Wiss <wassimbenameur@gmail.com>
2015-11-29 14:41:29 +0800

    map

--- js/app.js
+++ js/app.js
@@ -15,37 +15,38 @@ var app = angular.module('reddit-camouflage',[])
 }]).controller('overflowController',function($scope, $routeParams){
 	var url='https://www.reddit.com/.json?3jsonp=?'
 	$scope.posts=[];
-	if(localStorage.getItem("overflow_title"))
+
+  if(localStorage.getItem("overflow_title"))
 		$scope.overflow_title=localStorage.getItem("overflow_title");
 	else
 		$scope.overflow_title="The Definitive C++ Book Guide and List";
 
 	$.get(url,{},function(response) {
-	   	var subRedditsArray=response.data.children;
-		for (var i = 0; i < subRedditsArray.length; i++) {
-			$scope.posts.push({
-				title:subRedditsArray[i].data.title,
-				author:subRedditsArray[i].data.author,
-				subreddit:subRedditsArray[i].data.subreddit,
-				domain:subRedditsArray[i].data.domain,
-				permalink:subRedditsArray[i].data.permalink,
-				score:subRedditsArray[i].data.score,
-				type:subRedditsArray[i].data.post_hint,
-				thumbnail:{thumbnail_url:subRedditsArray[i].data.thumbnail,width:subRedditsArray[i].data.width,height:subRedditsArray[i].data.height},
-				num_comments:subRedditsArray[i].data.num_comments,
-				selfText:subRedditsArray[i].data.selftext,
-				selfTextShort:subRedditsArray[i].data.selftext.substr(0, 500),
-				expanded:false,
-				url:subRedditsArray[i].data.url});
-		}
-		$scope.$apply();
+      $scope.posts = response.data.children.map(function (post) {
+        return {
+          title:post.title,
+          author:post.author,
+          subreddit:post.subreddit,
+          domain:post.domain,
+          permalink:post.permalink,
+          score:post.score,
+          type:post.post_hint,
+          thumbnail:{thumbnail_url:post.thumbnail,width:post.width,height:post.height},
+          num_comments:post.num_comments,
+          selfText:post.selftext,
+          selfTextShort:post.selftext.substr(0, 500),
+          expanded:false,
+          url:post.url
+      };
     });
+		$scope.$apply();
+  });
 
 
 	$scope.editTitle =function() {
 		console.log("edit title");
-		document.getElementById('subPost_title_content').style.display="none";
-		document.getElementById('subPost_title_content_input').style.display="initial";
+		document.getElementById('subPost_title_content').style.display = "none";
+		document.getElementById('subPost_title_content_input').style.display = "initial";
 		document.getElementById('subPost_title_content_input').focus();
 	};
 
@@ -68,9 +69,10 @@ var app = angular.module('reddit-camouflage',[])
 		post.expanded = true;
 	}
 })
+
 .controller('overflowPostController',function($scope, $routeParams){
 	//ToDo  get reddit post from url params
-	var url='https://www.reddit.com/r/nosleep/comments/3uiif7/we_went_to_the_hospital_on_thanksgiving_and_the/.json?3jsonp=?'
+	var url='https://www.reddit.com/r/AskReddit/comments/3unb5b/.json?3jsonp=?'
 	$scope.posts = [];
 	$scope.comments = [];
 	if(localStorage.getItem("overflow_title"))
@@ -79,18 +81,23 @@ var app = angular.module('reddit-camouflage',[])
 		$scope.overflow_title = "The Definitive C++ Book Guide and List";
 
 	$.get(url,{},function(response) {
-	  var postCommments = response[1].data.children;
-	  for (var i = 0; i < postCommments.length; i++) {
-	  	//if(postCommments[i].data.author == "PM_ME_PAYPALMONEY")
-	  	$scope.comments.push({
-	  		author:postCommments[i].data.author,
-	  		body:postCommments[i].data.body,
-        replies:(postCommments[i].data.replies.data)?postCommments[i].data.replies.data.children:null,
-	  		score:postCommments[i].data.score
-	  	})
-	  };
+    console.log(response);
+    $scope.comments = response[1].data.children.map(function(comment) {
+      return {
+        author:comment.data.author,
+        body:comment.data.body,
+        score:comment.data.score,
+        replies:comment.data.replies ? comment.data.replies.data.children.map(function (reply) {
+          return {
+                  content:reply.data.body,
+                  author:reply.data.author,
+                  score:reply.data.score
+                };
+        }):null
+      }
+    })
 
-      var subRedditsArray = response[0].data.children;
+    var subRedditsArray = response[0].data.children;
       for (var i = 0; i < subRedditsArray.length; i++) {
   			$scope.posts.push({
   				title:subRedditsArray[i].data.title,

26eb68abf03f6b76c5704fc1e450d93c72dedb3d
L-Wiss <wassimbenameur@gmail.com>
2016-01-19 16:21:58 +0100

    hover over image

--- js/app.js
+++ js/app.js
@@ -35,7 +35,12 @@ angular.module('reddit-camouflage',[])
           permalink:post.data.permalink,
           score:post.data.score,
           type:post.data.post_hint,
-          thumbnail:{thumbnail_url:post.data.thumbnail,width:post.data.width,height:post.data.height},
+          thumbnail:post.data.thumbnail.indexOf("http") !== -1?{
+                      thumbnail_url:post.data.thumbnail,
+                      width:post.data.width,
+                      height:post.data.height,
+                      original:post.data.url
+                    }:null,
           num_comments:post.data.num_comments,
           selfText:post.data.selftext,
           selfTextShort:post.data.selftext.substr(0, 500),
@@ -49,42 +54,50 @@ angular.module('reddit-camouflage',[])
 
 	$scope.editTitle =function() {
 		console.log("edit title");
-		document.getElementById('subPost_title_content').style.display = "none";
-		document.getElementById('subPost_title_content_input').style.display = "initial";
-		document.getElementById('subPost_title_content_input').focus();
-	};
+    document.getElementById('subPost_title_content').style.display = "none";
+    document.getElementById('subPost_title_content_input').style.display = "initial";
+    document.getElementById('subPost_title_content_input').focus();
+  };
 
-	$scope.displayEdit =function() {
-		clearTimeout(hideTimeout);
-		document.getElementById("display_button").style.display="inline-block";
-	};
+  $scope.displayEdit =function() {
+    clearTimeout(hideTimeout);
+    document.getElementById("display_button").style.display="inline-block";
+  };
 
-	$scope.hideEdit =function() {
-		clearTimeout(hideTimeout);
-		hideTimeout=setTimeout(function(){document.getElementById("display_button").style.display="none";},1000);
-	};
+  $scope.hideEdit =function() {
+    clearTimeout(hideTimeout);
+    hideTimeout=setTimeout(function(){document.getElementById("display_button").style.display="none";},1000);
+  };
 
-	$scope.compressSelftText =  function(post)
-	{
-		post.expanded =  false;
-	}
+  $scope.compressSelftText =  function(post)
+  {
+    post.expanded =  false;
+  }
 
-	$scope.expandSelfText =  function (post) {
-		post.expanded = true;
-	}
+  $scope.expandHoveredContent = function(post) {
+    post.showHovered = true;
+    console.log(post.showHovered)
+  }
+
+  $scope.hideHoveredContent = function(post) {
+    post.showHovered = false;
+  }
+
+  $scope.expandSelfText =  function (post) {
+    post.expanded = true;
+  }
 })
 
 .controller('overflowPostController',function($scope, $routeParams){
-	
-	var url='https://www.reddit.com/r/'+$routeParams.subreddit+'/comments/'+$routeParams.postId+'/.json?3jsonp=?'
-	$scope.posts = [];
-	$scope.comments = [];
-	if(localStorage.getItem("overflow_title"))
-		$scope.overflow_title = localStorage.getItem("overflow_title");
-	else
-		$scope.overflow_title = "The Definitive C++ Book Guide and List";
+  var url='https://www.reddit.com/r/'+$routeParams.subreddit+'/comments/'+$routeParams.postId+'/.json?3jsonp=?'
+  $scope.posts = [];
+  $scope.comments = [];
+  if(localStorage.getItem("overflow_title"))
+    $scope.overflow_title = localStorage.getItem("overflow_title");
+  else
+    $scope.overflow_title = "The Definitive C++ Book Guide and List";
 
-	$.get(url,{},function(response) {
+  $.get(url,{},function(response) {
     console.log(response);
     $scope.comments = response[1].data.children.map(function(comment) {
       return {
@@ -103,44 +116,45 @@ angular.module('reddit-camouflage',[])
 
     var subRedditsArray = response[0].data.children;
       for (var i = 0; i < subRedditsArray.length; i++) {
-  			$scope.posts.push({
-  				title:subRedditsArray[i].data.title,
-  				author:subRedditsArray[i].data.author,
-  				subreddit:subRedditsArray[i].data.subreddit,
-  				domain:subRedditsArray[i].data.domain,
-  				permalink:subRedditsArray[i].data.permalink,
-  				score:subRedditsArray[i].data.score,
-  				type:subRedditsArray[i].data.post_hint,
-  				thumbnail:{thumbnail_url:subRedditsArray[i].data.thumbnail,width:subRedditsArray[i].data.width,height:subRedditsArray[i].data.height},
-  				num_comments:subRedditsArray[i].data.num_comments,
-  				selfText:subRedditsArray[i].data.selftext,
-  				selfTextShort:subRedditsArray[i].data.selftext.substr(0, 500),
-  				expanded:false,
-  				url:subRedditsArray[i].data.url});
-  		}
+        $scope.posts.push({
+          title:subRedditsArray[i].data.title,
+          author:subRedditsArray[i].data.author,
+          subreddit:subRedditsArray[i].data.subreddit,
+          domain:subRedditsArray[i].data.domain,
+          permalink:subRedditsArray[i].data.permalink,
+          score:subRedditsArray[i].data.score,
+          type:subRedditsArray[i].data.post_hint,
+          thumbnail:{thumbnail_url:subRedditsArray[i].data.thumbnail,width:subRedditsArray[i].data.width,height:subRedditsArray[i].data.height},
+          num_comments:subRedditsArray[i].data.num_comments,
+          selfText:subRedditsArray[i].data.selftext,
+          selfTextShort:subRedditsArray[i].data.selftext.substr(0, 500),
+          expanded:false,
+          url:subRedditsArray[i].data.url});
+      }
       $scope.$apply();
     });
 
-	$scope.editTitle = function() {
-		document.getElementById('subPost_title_content').style.display="none";
-		document.getElementById('subPost_title_content_input').style.display="initial";
-		document.getElementById('subPost_title_content_input').focus();
-	};
+  $scope.editTitle = function() {
+    document.getElementById('subPost_title_content').style.display="none";
+    document.getElementById('subPost_title_content_input').style.display="initial";
+    document.getElementById('subPost_title_content_input').focus();
+  };
 
-	$scope.displayEdit = function() {
-		clearTimeout(hideTimeout);
-		document.getElementById("display_button").style.display="inline-block";
-	};
+  $scope.displayEdit = function() {
+    clearTimeout(hideTimeout);
+    document.getElementById("display_button").style.display="inline-block";
+  };
 
-	$scope.hideEdit = function() {
-		clearTimeout(hideTimeout);
-		hideTimeout=setTimeout(function(){document.getElementById("display_button").style.display="none";},1000);
-	};
+  $scope.hideEdit = function() {
+    clearTimeout(hideTimeout);
+    hideTimeout=setTimeout(function(){document.getElementById("display_button").style.display="none";},1000);
+  };
 
-	$scope.compressSelftText = function(post)
-	{
-		post.expanded =  false;
-	}
+  $scope.compressSelftText = function(post) {
+    post.expanded =  false;
+  }
+
+
 
 	$scope.expandSelfText =  function (post) {
 		post.expanded = true;
